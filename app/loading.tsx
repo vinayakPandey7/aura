f532cbabd916ce4540b376599c40ab1aec2a9dae
pwvinayakpandey <vinayak.pandey+1@pw.live>
2024-06-14 15:27:10 +0530

    build fix

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import loadingLottieAnimation from "./assets/lottie/loadingAnimation.json";
-const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
-import dynamic from "next/dynamic";
+// import loadingLottieAnimation from "./assets/lottie/loadingAnimation.json";
+// const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
+// import dynamic from "next/dynamic";
 // const Lottie = dynamic(() => import('lottie-react'), { ssr: false });
 const LoadingScreen = () => {
   return (
     <div className="h-[100vh] w-[100vw] flex justify-center items-center">
       LOADING
-      <Lottie loop={true} animationData={loadingLottieAnimation} />
+      {/* <Lottie loop={true} animationData={loadingLottieAnimation} /> */}
       {/* <Lottie loop={true} animationData={loadingLottieAnimation} /> */}
     </div>
   );
